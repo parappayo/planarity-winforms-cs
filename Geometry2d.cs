087da6102d0a16d5391b13e1eb8ddaccc16dfa68
Jason Estey <esteyj@yahoo.com>
2020-10-16 12:31:38 -0700

    ported over alg and tests for slope, is-clockwise

--- Geometry2d.cs
+++ Geometry2d.cs
@@ -1,14 +1,41 @@
-using System.Drawing;
+using System;
 
 namespace BushidoBurrito.Planarity
 {
+    public struct Point<T>
+    {
+        public T X;
+        public T Y;
+
+        public Point(T x, T y)
+        {
+            this.X = x;
+            this.Y = y;
+        }
+    }
+
     public class Geometry2d
     {
-        static public int Slope(Point a, Point b)
+        static public float Slope(float a_x, float a_y, float b_x, float b_y)
         {
-            int dx = a.X - b.X;
-            if (dx == 0) { return 0; }
-            return (a.Y - b.Y) / dx;
+            var dx = a_x - b_x;
+            if (Math.Abs(dx) <= float.Epsilon) { return float.NaN; }
+            return (a_y - b_y) / dx;
+        }
+
+        static public float Slope(Point<float> a, Point<float> b)
+        {
+            return Slope(a.X, a.Y, b.X, b.Y);
+        }
+
+        static public bool IsClockwise(float a_x, float a_y, float b_x, float b_y, float c_x, float c_y)
+        {
+            return (b_y - a_y) * (c_x - a_x) > (c_y - a_y) * (b_x - a_x);
+        }
+
+        static public bool IsClockwise(Point<float> a, Point<float> b, Point<float> c)
+        {
+            return IsClockwise(a.X, a.Y, b.X, b.Y, c.X, c.Y);
         }
     }
 }
--- PlanarityTest/Geometry2d.cs
+++ PlanarityTest/Geometry2d.cs
@@ -1,4 +1,4 @@
-﻿using System.Drawing;
+﻿using System;
 using NUnit.Framework;
 using BushidoBurrito.Planarity;
 
@@ -7,12 +7,34 @@ namespace BushidoBurrito.PlanarityTest
     [TestFixture()]
     public class Geometry2dTest
     {
-        [Test()]
-        public void TestSlope()
+        [TestCase(0, 0, 1, 0, Result = 0)]
+        [TestCase(0, 0, 1, 1, Result = 1)]
+        [TestCase(1, 1, 0, 0, Result = 1)]
+        [TestCase(1, 1, 2, 2, Result = 1)]
+        [TestCase(-1, -1, -2, -2, Result = 1)]
+        [TestCase(0, 0, 1, 3, Result = 3)]
+        [TestCase(0, 0, -1, 3, Result = -3)]
+        [TestCase(1, 1, 2, -2, Result = -3)]
+        [TestCase(0, 0, 0, 1, Result = float.NaN)]
+        [TestCase(1, 1, 1, 0, Result = float.NaN)]
+        public float TestSlope(float a_x, float a_y, float b_x, float b_y)
         {
-            var slope = Geometry2d.Slope(new Point(0, 0), new Point(1, 1));
+            return Geometry2d.Slope(a_x, a_y, b_x, b_y);
+        }
 
-            Assert.AreEqual(slope, 1);
+        [TestCase(0, 0, 0, 0, 0, 0, Result = false)]
+        [TestCase(0, 0, 0, 0, 1, 1, Result = false)]
+        [TestCase(0, 0, 1, 1, 0, 0, Result = false)]
+        [TestCase(1, 1, 1, 0, 0, 0, Result = true)]
+        [TestCase(0, 0, 0, 0, 1, -1, Result = false)]
+        [TestCase(0, 0, 1, -1, 0, 0, Result = false)]
+        [TestCase(0, 0, 1, 0, 1, -1, Result = true)]
+        [TestCase(0, 0, 1, 0, 1, 1, Result = false)]
+        [TestCase(5, 5, 6, 4, 5, 3, Result = true)]
+        [TestCase(5, 5, 6, 4, 7, 3, Result = false)]
+        public bool TestIsClockwise(float a_x, float a_y, float b_x, float b_y, float c_x, float c_y)
+        {
+            return Geometry2d.IsClockwise(a_x, a_y, b_x, b_y, c_x, c_y);
         }
     }
 }
